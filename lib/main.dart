a43c6f896aa7ab55945a4c486f79622c3865d499
IRSAGE <sibo.egide98@gmail.com>
2024-06-27 13:38:08 +0200

    Simple Styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World!')));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  );
 }
