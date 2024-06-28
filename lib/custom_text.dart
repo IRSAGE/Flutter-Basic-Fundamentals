fd4867dba6c91d1037af1ee643a502c3bfcd548d
IRSAGE <sibo.egide98@gmail.com>
2024-06-28 11:44:46 +0200

    Reusable Gradient

--- lib/custom_text.dart
+++ lib/custom_text.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CustomText extends StatelessWidget {
   const CustomText(this.text, {super.key});
   final String text;
+
   @override
   Widget build(BuildContext context) {
     return Text(
